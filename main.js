e1d726be8c06728c77debdec44f1febdcbdab015
AshwiniChin <76168179+AshwiniChin@users.noreply.github.com>
2022-01-14 11:20:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,32 @@ function display() {
     var area = Number(length) * Number(length);
     var perimeter = 4 * Number(length);
 
-    document.getElementById("area").innerHTML = "Area is : " + area;
-    document.getElementById("perimeter").innerHTML = "Perimeter is : " + perimeter;
+    document.getElementById("area").innerHTML = "Area is : " + area +" sq. units";
+    document.getElementById("perimeter").innerHTML = "Perimeter is : " + perimeter + " units";
 }
+
+function square() {
+    var number = document.getElementById("number").value;
+
+
+    console.log("number= " + number);
+
+    var squareResult = Number(number) * Number(number);
+    
+
+    document.getElementById("square").innerHTML = "Square is : " + squareResult ;
+}
+
+function squareRoot() {
+    var root = document.getElementById("root").value;
+
+
+    console.log("root= " + root);
+
+    var rootResult = Math.sqrt(root);
+    
+
+    document.getElementById("rootDisplay").innerHTML = "Square root is : " + rootResult ;
+}
+
+
